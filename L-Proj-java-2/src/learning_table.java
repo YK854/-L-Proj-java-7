eb11ef16811c3977d4a0aa39e6580b81de8a6f93
YK854 <95330893+YK854@users.noreply.github.com>
2022-05-06 22:01:55 +0900

    first commit

---
+++ L-Proj-java-2/src/learning_table.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author taro
+ *
+ */
+public class learning_table {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
